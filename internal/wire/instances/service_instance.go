195d4174dcfdfcdbe33d485a707877a2a335b97e
trungnt1811 <trungqn.ptit@gmail.com>
2025-05-26 15:16:55 +0700

    refactor service instances

--- internal/wire/instances/service_instance.go
+++ internal/wire/instances/service_instance.go
@@ -9,101 +9,46 @@ import (
 )
 
 var (
-	serviceOnce    sync.Once
+	initServicesOnce sync.Once
+
 	lifeAIInstance services.LifeAIService
 	emailInstance  services.EmailService
 	smsInstance    services.SMSService
 	jwtInstance    services.JWTService
 )
 
-// LifeAIServiceInstance provides a singleton instance of LifeAIClient.
+func initServices() {
+	config := conf.GetConfiguration()
+
+	lifeAIEndpoint := config.LifeAIConfig.BackendURL
+	logger.GetLogger().Infof("Initializing service clients with LifeAI endpoint: %s", lifeAIEndpoint)
+
+	lifeAIInstance = services.NewLifeAIService(lifeAIEndpoint)
+	emailInstance = services.NewEmailService()
+	smsInstance = services.NewSMSService()
+	jwtInstance = services.NewJWTService(
+		config.JwtConfig.Secret,
+		config.JwtConfig.AccessLifetime,
+		config.JwtConfig.RefreshLifetime,
+	)
+}
+
 func LifeAIServiceInstance() services.LifeAIService {
-	serviceOnce.Do(func() {
-		// Get LifeAI endpoint from config
-		config := conf.GetConfiguration()
-		lifeAIEndpoint := config.LifeAIConfig.BackendURL
-
-		logger.GetLogger().Infof("Initializing LifeAI client with endpoint: %s", lifeAIEndpoint)
-
-		// Create service instances
-		lifeAIInstance = services.NewLifeAIService(lifeAIEndpoint)
-		emailInstance = services.NewEmailService()
-		smsInstance = services.NewSMSService()
-		jwtInstance = services.NewJWTService(
-			config.JwtConfig.Secret,
-			config.JwtConfig.AccessLifetime,
-			config.JwtConfig.RefreshLifetime,
-		)
-	})
-
+	initServicesOnce.Do(initServices)
 	return lifeAIInstance
 }
 
-// EmailServiceInstance provides a singleton instance of EmailService.
 func EmailServiceInstance() services.EmailService {
-	serviceOnce.Do(func() {
-		// Get email service configuration from config
-		config := conf.GetConfiguration()
-		lifeAIEndpoint := config.LifeAIConfig.BackendURL
-
-		logger.GetLogger().Infof("Initializing LifeAI client with endpoint: %s", lifeAIEndpoint)
-
-		// Create service instances
-		lifeAIInstance = services.NewLifeAIService(lifeAIEndpoint)
-		emailInstance = services.NewEmailService()
-		smsInstance = services.NewSMSService()
-		jwtInstance = services.NewJWTService(
-			config.JwtConfig.Secret,
-			config.JwtConfig.AccessLifetime,
-			config.JwtConfig.RefreshLifetime,
-		)
-	})
-
+	initServicesOnce.Do(initServices)
 	return emailInstance
 }
 
-// SMSServiceInstance provides a singleton instance of SMSService.
 func SMSServiceInstance() services.SMSService {
-	serviceOnce.Do(func() {
-		// Get SMS service configuration from config
-		config := conf.GetConfiguration()
-		lifeAIEndpoint := config.LifeAIConfig.BackendURL
-
-		logger.GetLogger().Infof("Initializing LifeAI client with endpoint: %s", lifeAIEndpoint)
-
-		// Create service instances
-		lifeAIInstance = services.NewLifeAIService(lifeAIEndpoint)
-		emailInstance = services.NewEmailService()
-		smsInstance = services.NewSMSService()
-		jwtInstance = services.NewJWTService(
-			config.JwtConfig.Secret,
-			config.JwtConfig.AccessLifetime,
-			config.JwtConfig.RefreshLifetime,
-		)
-	})
-
+	initServicesOnce.Do(initServices)
 	return smsInstance
 }
 
-// JWTServiceInstance provides a singleton instance of JWTService.
 func JWTServiceInstance() services.JWTService {
-	serviceOnce.Do(func() {
-		// Get JWT service configuration from config
-		config := conf.GetConfiguration()
-		lifeAIEndpoint := config.LifeAIConfig.BackendURL
-
-		logger.GetLogger().Infof("Initializing LifeAI client with endpoint: %s", lifeAIEndpoint)
-
-		// Create service instances
-		lifeAIInstance = services.NewLifeAIService(lifeAIEndpoint)
-		emailInstance = services.NewEmailService()
-		smsInstance = services.NewSMSService()
-		jwtInstance = services.NewJWTService(
-			config.JwtConfig.Secret,
-			config.JwtConfig.AccessLifetime,
-			config.JwtConfig.RefreshLifetime,
-		)
-	})
-
+	initServicesOnce.Do(initServices)
 	return jwtInstance
 }
