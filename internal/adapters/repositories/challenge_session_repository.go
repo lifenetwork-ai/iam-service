54da091fb966bfe907513cfc037a110fc4c5bd3c
Tuan Tran <tuan.tran@genefriendway.com>
2025-06-09 06:37:00 +0000

    test

--- internal/adapters/repositories/challenge_session_repository.go
+++ internal/adapters/repositories/challenge_session_repository.go
@@ -19,22 +19,7 @@ func NewChallengeSessionRepository(cache cachingTypes.CacheRepository) interface
 	}
 }
 
-// // Save challenge session to cache for 5 minutes
-// 	cacheKey := &cachingTypes.Keyer{Raw: sessionID}
-// 	cacheValue := challengeSession{
-// 		Type:  "email",
-// 		Email: email,
-// 		OTP:   otp,
-// 	}
-// 	if err := u.cacheRepo.SaveItem(cacheKey, cacheValue, 5*time.Minute); err != nil {
-// 		return nil, &dto.ErrorDTOResponse{
-// 			Status:  http.StatusInternalServerError,
-// 			Code:    "MSG_CACHING_FAILED",
-// 			Message: "Caching failed",
-// 			Details: []interface{}{err.Error()},
-// 		}
-// 	}
-
+// SaveChallenge saves a challenge session in the cache with a specified TTL.
 func (r *challengeSessionRepository) SaveChallenge(_ context.Context, sessionID string, challenge *domain.ChallengeSession, ttl time.Duration) error {
 	cacheKey := &cachingTypes.Keyer{Raw: sessionID}
 	if err := r.cache.SaveItem(cacheKey, challenge, ttl); err != nil {
@@ -43,6 +28,8 @@ func (r *challengeSessionRepository) SaveChallenge(_ context.Context, sessionID 
 	return nil
 }
 
+// GetChallenge retrieves a challenge session from the cache using the session ID.
+// If the session does not exist, it returns an error.
 func (r *challengeSessionRepository) GetChallenge(_ context.Context, sessionID string) (*domain.ChallengeSession, error) {
 	cacheKey := &cachingTypes.Keyer{Raw: sessionID}
 	var challenge domain.ChallengeSession
