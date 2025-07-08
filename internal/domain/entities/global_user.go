acb79ec1386c40a6d209813eb10404dcacf498a4
trungnt1811 <trungqn.ptit@gmail.com>
2025-07-08 11:50:02 +0700

    refactor: update global user

--- internal/domain/entities/global_user.go
+++ internal/domain/entities/global_user.go
@@ -6,9 +6,10 @@ import (
 
 // Represent a global user in the IAM system.
 type GlobalUser struct {
-	ID        string    `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	ID         string         `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Identities []UserIdentity `json:"identities" gorm:"foreignKey:GlobalUserID;references:ID"`
+	CreatedAt  time.Time      `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt  time.Time      `json:"updated_at" gorm:"autoUpdateTime"`
 }
 
 // TableName overrides the default table name for GORM.
