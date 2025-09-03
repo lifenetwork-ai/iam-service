7e9d7438a6a4e5b74436270daf2b1babd4c379c9
trungnt1811 <trungqn.ptit@gmail.com>
2025-09-03 10:36:22 +0700

    upadate domain model

--- internal/domain/entities/user_identity.go
+++ internal/domain/entities/user_identity.go
@@ -8,6 +8,7 @@ import (
 type UserIdentity struct {
 	ID           string    `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	GlobalUserID string    `json:"global_user_id" gorm:"type:uuid;not null"`
+	TenantID     string    `json:"tenant_id" gorm:"type:uuid;not null"`
 	Type         string    `json:"type" gorm:"type:varchar(20);not null"` // email, phone, google, wallet, etc.
 	Value        string    `json:"value" gorm:"type:varchar(255);not null"`
 	CreatedAt    time.Time `json:"created_at" gorm:"autoCreateTime"`
