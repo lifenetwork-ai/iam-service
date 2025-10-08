8f4e0c3c2be1271eabada27added49a2d438780f
trungnt1811 <trungqn.ptit@gmail.com>
2025-10-08 12:30:49 +0700

    feat: implement rate limit per ip

--- constants/rate_limit.go
+++ constants/rate_limit.go
@@ -7,3 +7,9 @@ const (
 	MaxAttemptsPerWindow = 5
 	RateLimitWindow      = 5 * time.Minute
 )
+
+// Actions for rate limiting
+const (
+	LoginWithPhoneAction = "login_phone"
+	LoginWithEmailAction = "login_email"
+)
